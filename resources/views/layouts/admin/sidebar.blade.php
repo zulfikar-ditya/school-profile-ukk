58513a1326403b827847e083d0b119927b01159b
zulfikar-dityaa <zulfikarditya.work@gmail.com>
2022-02-27 14:00:54 +0700

    update sidebar darkmode

--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -7,7 +7,7 @@
 <nav class="md:left-0 md:block md:fixed md:top-0 md:bottom-0 md:overflow-y-auto md:flex-row md:flex-nowrap md:overflow-hidden shadow-xl bg-white dark:bg-slate-800 dark:text-slate-200 flex flex-wrap items-center justify-between relative md:w-64 z-10 py-4 px-6">
     <div
         class="md:flex-col md:items-stretch md:min-h-full md:flex-nowrap px-0 flex flex-wrap items-center justify-between w-full mx-auto">
-        <button class="cursor-pointer text-black opacity-50 md:hidden px-3 py-1 text-xl leading-none bg-transparent rounded border border-solid border-transparent" type="button"  onclick="toggleNavbar('example-collapse-sidebar')"> 
+        <button class="cursor-pointer text-black dark:text-gray-200 opacity-50 md:hidden px-3 py-1 text-xl leading-none bg-transparent rounded border border-solid border-transparent" type="button"  onclick="toggleNavbar('example-collapse-sidebar')"> 
             <i class="" data-feather="menu"></i>
         </button> 
         <a class="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0" href="{{route('admin.index')}}">
@@ -18,12 +18,12 @@
             <li class="inline-block relative"> 
                 <a class="text-blueGray-500 block" href="#pablo" onclick="openDropdown(event,'user-responsive-dropdown')">
                     <div class="items-center flex"> 
-                        <span class="w-12 h-12 text-sm text-white bg-blueGray-200 inline-flex items-center justify-center rounded-full">
+                        <span class="w-12 h-12 text-sm text-white  inline-flex items-center justify-center rounded-full">
                             <img alt="..." class="w-full rounded-full align-middle border-none shadow-lg" src="{{Auth::user()->profile_photo_url}}" />
                         </span>
                     </div>
                 </a>
-                <div class="hidden bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg mt-1" style="min-width: 12rem;" id="user-responsive-dropdown">                     
+                <div class="hidden bg-white dark:bg-gray-800 dark:border dark:border-gray-300 text-base z-50 float-left py-2 list-none text-left rounded shadow-lg mt-1" style="min-width: 12rem;" id="user-responsive-dropdown">                     
                     <a href="{{route('dashboard')}}" class="text-sm py-2 px-4 font-normal w-full whitespace-nowrap bg-transparent text-blueGray-700 flex items-center">
                         <i class=""data-feather="user"></i>
                         My Account
@@ -39,14 +39,14 @@
                 </div>
             </li>
         </ul>
-        <div class="md:flex md:flex-col md:items-stretch md:opacity-100 md:relative md:mt-4 md:shadow-none shadow absolute top-0 left-0 right-0 z-40 overflow-y-auto overflow-x-hidden h-auto items-center flex-1 rounded hidden" id="example-collapse-sidebar">
+        <div class="md:flex md:flex-col md:items-stretch md:opacity-100 md:relative md:mt-4 md:shadow-none shadow absolute top-0 left-0 right-0 z-40 overflow-y-auto overflow-x-hidden h-auto items-center flex-1 rounded hidden bg-white dark:bg-slate-800" id="example-collapse-sidebar">
             <div class="md:min-w-full md:hidden block pb-4 mb-4 border-b border-solid border-blueGray-200">
                 <div class="flex flex-wrap">
                     <div class="w-6/12"> 
-                        <a class="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0" href="javascript:void(0)"> School Profile</a> 
+                        <a class="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0" href="{{route('admin.index')}}">{{$name}}</a> 
                     </div>
                     <div class="w-6/12 flex justify-end"> 
-                        <button type="button" class="cursor-pointer text-black opacity-50 md:hidden px-3 py-1 text-xl leading-none bg-transparent rounded border border-solid border-transparent" onclick="toggleNavbar('example-collapse-sidebar')"> 
+                        <button type="button" class="cursor-pointer text-black dark:text-white opacity-50 md:hidden px-3 py-1 text-xl leading-none bg-transparent rounded border border-solid border-transparent" onclick="toggleNavbar('example-collapse-sidebar')"> 
                             <i data-feather="x"></i> 
                         </button>
                     </div>
