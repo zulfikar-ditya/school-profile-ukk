ff4abbabc3c2d7dd1d2838cc096a27b9988fe09f
zulfikar-dityaa <zulfikarditya.work@gmail.com>
2022-03-03 21:22:17 +0700

    navbar + footer landing

--- resources/views/layouts/frontend/footer.blade.php
+++ resources/views/layouts/frontend/footer.blade.php
@@ -42,7 +42,7 @@
                 </ul>
             </div>
             <div>
-                <h3 class="mb-6 text-sm font-semibold text-gray-400 uppercase">Address</h3>
+                <h3 class="mb-6 text-sm font-semibold text-gray-300 uppercase">Address</h3>
                 <ul>
                     <li class="mb-4">
                         {{$school_information->address}}
