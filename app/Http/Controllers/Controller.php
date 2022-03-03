73fcf9622b9eb2b0724c491c48bc3dbe7a7e9873
zulfikar-dityaa <zulfikarditya.work@gmail.com>
2022-03-03 20:00:17 +0700

    try deployment fix

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,13 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Components\Pack;
+use GuzzleHttp\Psr7\UploadedFile;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Storage;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests, Pack;
-    use \App\Components\UploadFile;
+    // use \App\Components\UploadFile;
+    public function uploadFile(UploadedFile $uploadedFile, $folder = null, $disk = 'public')
+    {
+        $file = Storage::disk($disk)->put("$folder", $uploadedFile);
+        return $file;
+    }
+
+    public function deleteOne($filename, $disk = 'public',)
+    {
+        Storage::disk($disk)->delete($filename);
+    }
 }
