dc0853b2cd5bc69be3383afaa5211829061f604e
zulfikar-dityaa <zulfikarditya.work@gmail.com>
2022-03-05 11:14:19 +0700

    minify assets css and js

--- public/js/loader.js
+++ public/js/loader.js
@@ -1,11 +1,1 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!********************************!*\
-  !*** ./resources/js/loader.js ***!
-  \********************************/
-window.addEventListener("load", function () {
-  var loader = document.querySelector("#loader");
-  loader.classList.add("hidden");
-});
-/******/ })()
-;
+window.addEventListener("load",(function(){document.querySelector("#loader").classList.add("hidden")}));
